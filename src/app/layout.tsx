21926a31044c78120ef2444b213c6c0a7211e2f5
imaimai17468 <tosiki_801_life@outlook.jp>
2025-01-02 18:11:13 +0900

    Refactor layout and remove ThemeProvider component
    - Removed the ThemeProvider component and its associated types to simplify the layout structure.
    - Updated RootLayout to apply a dark class directly to the body, enhancing theme management.
    - Deleted unused Header and ModeToggle components to streamline the codebase.
    - Improved overall component organization by eliminating unnecessary dependencies.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { MainLayout } from "@/components/layout/main-layout";
-import { ThemeProvider } from "@/components/layout/theme-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,10 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <MainLayout>{children}</MainLayout>
-        </ThemeProvider>
+      <body className="dark">
+        <MainLayout>{children}</MainLayout>
       </body>
     </html>
   );
--- src/components/parts/header.tsx
+++
@@ -1,9 +0,0 @@
-import { ModeToggle } from "./mode-toggle";
-
-export const Header: React.FC = () => {
-  return (
-    <div className="absolute top-0 right-0 mr-8 mt-4 flex justify-end">
-      <ModeToggle />
-    </div>
-  );
-};
